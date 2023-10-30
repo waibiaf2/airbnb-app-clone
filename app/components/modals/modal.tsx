605593ac82a44e9d3ab6d8c5ca637a2290c6f263
Waibi Andrew Franlin <waibiaf2@gmail.com>
2023-10-30 21:59:00 +0300

    Add skelton for the model component

---
+++ app/components/modals/modal.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import React, {useCallback, useEffect, useState} from 'react';
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement,
+    actionLabel: string
+    disabled?:boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string
+}
+
+const Modal:React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryAction,
+    secondaryLabel
+                                    }) => {
+    const [showModal, setShowModal] = useState(isOpen);
+
+    useEffect(() => {
+        setShowModal(isOpen)
+    }, [isOpen]);
+
+    const handleClose = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        setShowModal(false);
+        setTimeout(()=>{
+            onClose();
+        }, 300)
+    },[disabled, onClose])
+
+    const handleSubmit = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+        onSubmit();
+    }, [disabled, onSubmit]);
+
+    const handleSecondaryAction = useCallback(()=>{
+        if (disabled || !secondaryAction) {
+            return
+        }
+        secondaryAction();
+    },[disabled,secondaryAction])
+
+    if (!isOpen) {
+        return null;
+    }
+
+    return (
+        <>
+            <div className="
+                    justify-center
+                    items-center
+                    flex
+                    overflow-x-hidden
+                    overflow-y-auto
+                    fixed
+                    inset-0
+                    outline-none
+                    focus:outline-none
+                    bg-neutral-800/70
+                "
+            >
+                <div className="
+                        relative
+                        w-full
+                        md:w-4/6
+                        lg:w-3/6
+                        xl:basis-2/5
+                        my-6
+                        mx-auto
+                        h-fulll
+                        lg:h-auto
+                        md:h-auto
+                    "
+                >
+                    {/*Content*/}
+                    <div
+                        className={`
+                            translate
+                            duration-300
+                            h-full
+                            ${showModal ? 'translate-y-0':'translate-y-full'}
+                            ${showModal ? 'opacity-100':'opacity-0'} 
+                        `}
+                    >
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Modal;
